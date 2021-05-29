af37d867a060e34d80d0f34c8863824bfa0f8d71
lucifer <xiaoweliang@qq.com>
2021-05-29 22:13:02 +0800

    登录页面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,62 @@
 <template>
 <div>
-  Login!
+<!--  v-model:rules=:rules-->
+  <el-form :rules="rules" :model="loginForm" class="loginContainer">
+    <h3 class="login-title">系统登录</h3>
+
+    <el-form-item prop="username">
+      <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入你的用户名"></el-input>
+    </el-form-item>
+
+    <el-form-item prop="password">
+      <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
+    </el-form-item>
+
+    <el-checkbox class="loginRemember" v-model="checked"></el-checkbox>
+
+    <el-button type="primary" style="width:100%;">登录</el-button>
+  </el-form>
 </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data(){
+    return {
+      loginForm:{
+        username:'admin',
+        password:'123'
+      },
+      checked: true,
+      rules:{
+        username:[{required: true, message:"请输入你的用户名",trigger:"blur"}],
+        password:[{required: true, message:"请输入密码",trigger:"blur"}]
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
+.loginContainer{
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 180px auto;
+  width:350px;
+  padding:35px 35px 15px 35px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+}
+.login-title{
+  margin:10px auto 20px auto;
+  text-align:center;
+  color: #000000;
+}
+.loginRemember{
+  text-align:left;
+  margin:0 0 15px 0;
+}
 
 </style>
