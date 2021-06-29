d8190c59584c216f5eb0cc14df21637a5214e87c
lucifer <xiaoweliang@qq.com>
2021-06-29 20:18:23 +0800

    高级搜索功能细化

--- src/views/emp/EmpBasic.vue
+++ src/views/emp/EmpBasic.vue
@@ -1,7 +1,8 @@
 <template>
 
   <div>
-  <div style="display: flex;justify-content: space-between">
+  <div>
+    <div style="display: flex;justify-content: space-between">
     <div>
       <el-input placeholder="请输入员工名进行搜索，可以直接回车搜索..." prefix-icon="el-icon-search"
                 clearable
@@ -20,7 +21,6 @@
         高级搜索
       </el-button>
     </div>
-
     <div>
       <el-upload
           :show-file-list="false"
@@ -41,7 +41,110 @@
         添加用户
       </el-button>
     </div>
+    </div>
+    <transition name="slide-fade">
+    <div v-show="showAdvanceSearchView"
+        style="border: 1px solid #409eff;border-radius: 5px;box-sizing: border-box;padding: 5px;margin: 10px 0px;">
+      <el-row>
+        <el-col :span="5">
+          政治面貌:
+          <el-select v-model="searchValue.politicId" placeholder="政治面貌" size="mini"
+                     style="width: 130px;">
+            <el-option
+                v-for="item in politicsstatus"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id">
+            </el-option>
+          </el-select>
+        </el-col>
+        <el-col :span="4">
+          民族:
+          <el-select v-model="searchValue.nationId" placeholder="民族" size="mini"
+                     style="width: 130px;">
+            <el-option
+                v-for="item in nations"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id">
+            </el-option>
+          </el-select>
+        </el-col>
+        <el-col :span="4">
+          职位:
+          <el-select v-model="searchValue.posId" placeholder="职位" size="mini" style="width: 130px;">
+            <el-option
+                v-for="item in positions"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id">
+            </el-option>
+          </el-select>
+        </el-col>
+        <el-col :span="4">
+          职称:
+          <el-select v-model="searchValue.jobLevelId" placeholder="职称" size="mini"
+                     style="width: 130px;">
+            <el-option
+                v-for="item in joblevels"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id">
+            </el-option>
+          </el-select>
+        </el-col>
+        <el-col :span="7">
+          聘用形式:
+          <el-radio-group v-model="searchValue.engageForm">
+            <el-radio label="劳动合同">劳动合同</el-radio>
+            <el-radio label="劳务合同">劳务合同</el-radio>
+          </el-radio-group>
+        </el-col>
+      </el-row>
+
+      <el-row style="margin-top: 10px">
+        <el-col :span="5">
+          所属部门:
+          <el-popover
+              placement="right"
+              title="请选择部门"
+              width="200"
+              trigger="manual"
+              v-model="popVisible">
+            <el-tree default-expand-all :data="allDeps" :props="defaultProps"
+                     @node-click="searvhViewHandleNodeClick"></el-tree>
+            <div slot="reference"
+                 style="width: 130px;display: inline-flex;font-size: 13px;border: 1px solid #dedede;height: 26px;border-radius: 5px;cursor: pointer;align-items: center;padding-left: 8px;box-sizing: border-box;margin-left: 3px"
+                 @click="showDepView">{{inputDepName}}
+            </div>
+          </el-popover>
+        </el-col>
+        <el-col :span="10">
+          入职日期:
+          <el-date-picker
+              v-model="searchValue.beginDateScope"
+              type="daterange"
+              size="mini"
+              unlink-panels
+              value-format="yyyy-MM-dd"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期">
+          </el-date-picker>
+        </el-col>
+        <el-col :span="5" :offset="4">
+          <el-button size="mini" @click="showAdvanceSearchView = !showAdvanceSearchView">取消</el-button>
+          <el-button size="mini" icon="el-icon-search" type="primary" @click="initEmps('advanced')">搜索</el-button>
+          <el-button size="mini" type="danger" @click="resetForm">重置</el-button>
+        </el-col>
+      </el-row>
+
+
+    </div>
+    </transition>
   </div>
+
+
 
 
     <div style="margin-top: 10px">
@@ -492,6 +595,7 @@ export default {
       popVisible: false,
       dialogVisible: false,
       allDeps: [],
+      showAdvanceSearchView: false,
       defaultProps: {
         children: 'children',
         label: 'name'
@@ -652,6 +756,16 @@ export default {
           this.total = resp.total;
         }
       });
+    },
+    resetForm() {
+      this.searchValue.politicId = null;
+      this.searchValue.nationId = null;
+      this.searchValue.jobLevelId = null;
+      this.searchValue.posId = null;
+      this.searchValue.engageForm = null;
+      this.searchValue.beginDateScope = null;
+      this.inputDepName = '请选择所属部门';
+
     },
     currentChange(currentPage) {
       this.page = currentPage;
@@ -843,11 +957,34 @@ export default {
       this.importDataDisabled = true;
     },
 
+    searvhViewHandleNodeClick(data) {
+      this.inputDepName = data.name;
+      this.searchValue.departmentId = data.id;
+      this.popVisible = !this.popVisible;
+    },
+
 
   }
 }
 </script>
 
 <style scoped>
+
+/* 可以设置不同的进入和离开动画 */
+/* 设置持续时间和动画函数 */
+.slide-fade-enter-active {
+  transition: all .5s ease;
+}
+
+.slide-fade-leave-active {
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+
+.slide-fade-enter, .slide-fade-leave-to
+  /* .slide-fade-leave-active for below version 2.1.8 */
+{
+  transform: translateX(10px);
+  opacity: 0;
+}
 
 </style>
