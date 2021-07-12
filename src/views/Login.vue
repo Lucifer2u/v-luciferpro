02e74ddafd3ed918e4a6d95a49a57dba0f9346e7
lucifer <xiaoweliang@qq.com>
2021-07-12 16:49:21 +0800

    添加验证码登录

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -17,7 +17,14 @@
     </el-form-item>
 
     <el-form-item prop="password">
-      <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码" @keydown.enter.native="submitLogin"></el-input>
+      <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
+    </el-form-item>
+
+
+    <el-form-item prop="code">
+      <el-input size="normal" type="text" v-model="loginForm.code" auto-complete="off"
+                placeholder="点击图片更换验证码" @keydown.enter.native="submitLogin" style="width: 250px"></el-input>
+      <img :src="vcUrl" @click="updateVerifyCode" alt="" style="cursor: pointer;">
     </el-form-item>
 
     <el-checkbox class="loginRemember" v-model="checked"></el-checkbox>
@@ -33,19 +40,25 @@ export default {
   name: "Login",
   data(){
     return {
+      vcUrl: '/verifyCode?time='+new Date(),
       loading: false,
       loginForm:{
         username:'admin',
-        password:'123'
+        password:'123',
+        code:''
       },
       checked: true,
       rules:{
         username:[{required: true, message:"请输入你的用户名",trigger:"blur"}],
-        password:[{required: true, message:"请输入密码",trigger:"blur"}]
+        password:[{required: true, message:"请输入密码",trigger:"blur"}],
+        code: [{required: true, message: '请输入验证码', trigger: 'blur'}]
       }
     }
   },
   methods:{
+    updateVerifyCode() {
+      this.vcUrl = '/verifyCode?time='+new Date();
+    },
     submitLogin(){
       this.$refs.loginForm.validate((valid) => {
         if (valid) {
@@ -87,9 +100,9 @@ export default {
   text-align:center;
   color: #000000;
 }
-.loginRemember{
-  text-align:left;
-  margin:0 0 15px 0;
+.loginRemember {
+  text-align: left;
+  margin: 0px 0px 15px 0px;
 }
 
 </style>
