575b82aafb74f605ad208aa2200324c051eb4575
lucifer <xiaoweliang@qq.com>
2021-07-12 20:09:45 +0800

    开启gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 let proxyObj = {};
+const CompressionPlugin = require("compression-webpack-plugin");
 proxyObj ['/'] = {
     ws:false,
     target:'http://localhost:8081',
@@ -17,5 +18,18 @@ module.exports={
         host:'localhost',
         port:8080,
         proxy:proxyObj
+    },
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            return {
+                plugins: [
+                    new CompressionPlugin({
+                        test: /\.js$|\.html$|\.css/,
+                        threshold: 1024,
+                        deleteOriginalAssets: false
+                    })
+                ]
+            }
+        }
     }
 }
